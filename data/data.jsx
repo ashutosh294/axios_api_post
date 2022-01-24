053ccd43edb717552ac3a58ec92d208322dcead8
ashutosh294 <89760486+ashutosh294@users.noreply.github.com>
2022-01-24 17:17:11 +0530

    Update data.jsx

--- data/data.jsx
+++ data/data.jsx
@@ -47,6 +47,65 @@ export function Data() {
 }
 
 
+
+// using usequery
+
+
+// import { useQuery } from "react-query"
+
+// import axios from "axios"
+
+
+
+// const fetchData = () => {
+
+//     return axios.get(' https://jsonplaceholder.typicode.com/posts')
+
+// }
+
+// export const Data = () => {
+
+//     const { data, isLoading } = useQuery('posts',fetchData)
+
+
+
+//     if (isLoading) {
+
+//         return <h2> Loading Data.. </h2>
+
+//     }
+
+
+
+//     return (
+
+//         <>
+
+//         <div>
+
+//             {data.data.map((user,index) => (
+
+//                 <div key = {index}>title = {user.title}</div>
+
+//             ))}
+
+//         </div>
+
+//         </>
+
+//     )
+
+// }
+
+
+
+
+
+
+
+
+
+
 // alternative method
 // import react,{useState,useEffect} from "react"
 
