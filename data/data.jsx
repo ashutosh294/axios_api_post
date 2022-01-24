b8d1a7c7a4c497a7ee9b52bd8d8c499ab6c16b22
ashutosh294 <89760486+ashutosh294@users.noreply.github.com>
2022-01-24 12:30:36 +0530

    Update data.jsx

--- data/data.jsx
+++ data/data.jsx
@@ -45,3 +45,48 @@ export function Data() {
         </div>
     )  
 }
+
+
+// alternative method
+// import react,{useState,useEffect} from "react"
+
+// import axios from "axios"
+
+
+
+// export function Data() {
+
+//     const[post,setPost] = useState([])
+
+//     const getRepo = () => {
+
+//         axios.get('https://jsonplaceholder.typicode.com/posts')
+
+//         .then((response) => {
+
+            
+
+//             const myRepo = response.data
+
+//             setPost(myRepo)
+
+//         })
+
+//     }
+
+//     useEffect(() => getRepo(),[])
+
+//     return(
+
+//         <div>
+
+//         {post.map((posts,index) =>(
+
+//             <div key = {index}>id = {posts.id}: title : {posts.title}</div>
+
+//         ))}
+
+//     </div>
+//     )
+
+// }
